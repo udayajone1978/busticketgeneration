5941226d90c10f16564d644af9ddf0a26443541f
udayajone1978 <udaya@market-intellect.com>
2022-10-14 17:09:12 +0530

    commitemail14thoctober

--- onlinebus/forms.py
+++ onlinebus/forms.py
@@ -1,5 +1,8 @@
+from socket import socket
+
 from django import forms
 from onlinebus.models import bus,consumer
+
 class busform(forms.ModelForm):
     class Meta:
         model = bus
@@ -10,10 +13,11 @@ class consumerform(forms.ModelForm):
         model= consumer
         fields='__all__'
 
+
 class contactform(forms.Form):
     name=forms.CharField(max_length=225)
     age=forms.IntegerField()
-    email=forms.EmailField()
+    email=forms.EmailField(required=True)
     address=forms.CharField(widget=forms.Textarea)
     contact_no=forms.IntegerField()
     start= forms.CharField()
